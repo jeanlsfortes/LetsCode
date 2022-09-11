eac6ac3fc1b6e140a6b5b12a690418542744dd84
jeanls-solano <jeanlucaseh10@gmail.com>
2022-09-11 19:57:29 -0400

    exercicios do uri atualizando comnetario

--- aula1/src/IdaAFeira.java
+++ aula1/src/IdaAFeira.java
@@ -1,20 +1,39 @@
 import java.util.Scanner;
 
 //ex uri https://www.beecrowd.com.br/judge/pt/problems/view/1281
+
+//.contains() = verificar se tem o elemnto que vc definir
+
 public class IdaAFeira {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
         for (int i = 0; i <N; i++){
+            String entrada;
             int M = scan.nextInt();
             String[] productsName = new String[M];
             float[] products = new float[M];
+            //clear scan
             scan.nextLine();
             for (int j=0; j< M;j++){
-                productsName[i] = scan.nextLine();
-                //products[i] = Integer.parseInt(productsName[i].substring(productsName[i].indexOf(" "),productsName[i].length()));
-                System.out.println(productsName[i].substring((productsName[i].indexOf(" ") + 1),productsName[i].length()));
+                entrada = scan.nextLine();
+                productsName[j] = entrada.substring(0,entrada.indexOf(" "));
+                products[j] = Float.parseFloat(entrada.substring((entrada.indexOf(" ") + 1),entrada.length()));
+                System.out.println(productsName[j] + " " + products[j]);
             }
+
+            int P = scan.nextInt();
+            String[] productsAmount = new String[P];
+            int[] productsNumber = new int[P];
+            scan.nextLine();
+            for (int j=0; j< P;j++){
+                entrada = scan.nextLine();
+                productsAmount[j] = entrada.substring(0,entrada.indexOf(" "));
+                productsNumber[j] = Integer.parseInt(entrada.substring((entrada.indexOf(" ") + 1),entrada.length()));
+                System.out.println(productsAmount[j] + " " + productsNumber[j]);
+            }
+
+
         }
 
     }
